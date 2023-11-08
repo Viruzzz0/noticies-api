6d7f65e1eb65689dfeeee869507f79ca63fc061f
CodeSandbox <support@codesandbox.io>
2023-11-08 03:25:39 +0000

    Sandbox snapshot commit

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-console.log("Hello CodeSandbox");
+import cheerio from "cheerio";
+
+async function getNoticie(name) {
+  const newsName = name.split(" ").join("+");
+  console.log("NewsName: ", newsName);
+
+  const url = `https://www.bing.com/news/search?q=${newsName}&qs=n&form=QBNT&sp=-1&lq=0&pq=persona+5&sc=10-9&sk=&cvid=621A2A47EB7247F591B0AFEF91B3692E&ghsh=0&ghacc=0&ghpl=`;
+
+  const customHeaders = {
+    "Accept-Language": "es",
+  };
+
+  const requestOptions = {
+    headers: customHeaders,
+    method: "GET",
+  };
+
+  const html = await fetch(url, requestOptions)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.text();
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+
+  const $ = cheerio.load(html);
+  const divContent = $("#algocore").find("div.news-card");
+  const noticies = [];
+
+  divContent.each((index, element) => {
+    const title = $(element).find("a.title").text();
+    const content = $(element).find("div.snippet").text();
+    const url = $(element).attr("url");
+    const imageUrl = $(element)
+      .find("div.image")
+      .find("img")
+      .attr("data-src-hq");
+    console.log("title: ", title);
+    console.log("----------");
+
+    noticies.push({ title, content, url, imageUrl });
+  });
+
+  return noticies;
+}
+
+const listNoticias = await getNoticie("persona 5");
+console.log(listNoticias);
